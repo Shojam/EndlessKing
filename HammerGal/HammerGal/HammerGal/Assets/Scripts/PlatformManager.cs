b20a94120b237fcc3859fdca316dbe03d340d62c
Shojam <jaimepinedamartinez@gmail.com>
2017-06-12 12:22:45 -0500

    Cambio a nueva version

---
+++ HammerGal/HammerGal/HammerGal/Assets/Scripts/PlatformManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformManager : MonoBehaviour {
+
+	// Use this for initialization
+
+    public bool rotate;
+	public bool activate;
+	public float speed;
+    private int degrees;
+	private int rotatingDir;
+	public Transform destination;
+	public Transform sourcePlatform;
+	void Start () {
+		if (rotate) {
+			StartCoroutine ("movePlatform");
+		}
+		if (activate) {
+			GetComponentInChildren<GemManager> ().OnActivate += startPlatform;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+	private void startPlatform()
+	{
+		StartCoroutine ("activatePlatform");
+	}
+
+	private IEnumerator activatePlatform()
+	{
+		while (Vector3.Distance (sourcePlatform.position, destination.position) > 1) {
+			sourcePlatform.position = Vector3.MoveTowards (sourcePlatform.position, destination.position, speed * Time.deltaTime);
+			yield return new WaitForEndOfFrame ();
+		}
+	}
+
+	private IEnumerator movePlatform()
+	{
+		degrees = 0;
+		rotatingDir = -1;
+		while (true) {
+			transform.Rotate (Vector3.back, speed * Time.deltaTime * rotatingDir, Space.World);
+			if (Mathf.Abs (transform.rotation.eulerAngles.z) > 179 && rotatingDir<0) {
+				yield return new WaitForSeconds (0.5f);
+				rotatingDir = 1;
+			}
+			if ((Mathf.Abs (transform.rotation.eulerAngles.z) <= 1 && rotatingDir>0) || Mathf.Abs (transform.rotation.eulerAngles.z) > 200 && rotatingDir>0) {
+				yield return new WaitForSeconds (1.5f);
+				rotatingDir = -1;
+			}
+
+			yield return new WaitForEndOfFrame ();
+
+		}
+	}    
+}
---
+++ HammerGal/HammerGal/HammerGal/Assets/Scripts/SwordManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordManager : MonoBehaviour {
+
+	private SpriteRenderer sprite;
+	private BoxCollider2D col;
+	private Animator anim;
+	// Use this for initialization
+	void Awake () {
+		sprite = GetComponent<SpriteRenderer> ();
+		sprite.enabled = false;
+		anim = GetComponent<Animator> ();
+		col = GetComponent<BoxCollider2D> ();
+		col.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (anim.GetCurrentAnimatorStateInfo (0).IsName ("Slice")) {
+			sprite.enabled = false;
+			col.enabled = false;
+		}
+	}
+
+	public void swing()
+	{
+		sprite.enabled = true;
+		col.enabled = true;
+		anim.SetTrigger ("Swing");
+	}
+}
